3cc9ddb139f08179554c7ec684f1d69eb84b777b
c.winger <c.winger@oeko.de>
2024-03-27 16:05:14 +0100

    removed old exports

--- index.mjs
+++ index.mjs
@@ -4,18 +4,8 @@ import { DataGraph } from "./modules/dataGraph.mjs";
 import { App } from "./modules/app.mjs";
 import {
   Component,
-  IntInputComponent,
-  LabelOutputComponent,
   InputComponent,
   OutputComponent,
 } from "./components/index.mjs";
 
-export {
-  App,
-  DataGraph,
-  Component,
-  IntInputComponent,
-  LabelOutputComponent,
-  InputComponent,
-  OutputComponent,
-};
+export { App, DataGraph, Component, InputComponent, OutputComponent };
